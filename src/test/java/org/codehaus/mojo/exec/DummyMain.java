5bd57c02572943ebc2c6f6d3202eb8c56874b051
Jerome Lacoste <jerome.lacoste@gmail.com>
2006-04-11 09:21:38 +0000

    MEXEC-3: integration tests for the exec:java mojo.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/exec-maven-plugin@1811 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/test/java/org/codehaus/mojo/exec/DummyMain.java
@@ -0,0 +1,20 @@
+package org.codehaus.mojo.exec;
+
+/**
+ * Simple class with a main method to call from unit tests
+ * @author Jerome Lacoste <jerome@coffeebreaks.org>
+ * @version $Id$
+ */
+class DummyMain {
+    /**
+     * Prints Hello followed by each argument, then a new line. Use a space character as separator.
+     * @param args
+     */
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer("Hello");
+        for (int i = 0; i < args.length; i++) {
+            buffer.append(' ').append(args[i]);
+        }
+        System.out.println(buffer.toString());
+    }
+}
---
+++ src/test/java/org/codehaus/mojo/exec/ExecJavaMojoTest.java
@@ -0,0 +1,82 @@
+package org.codehaus.mojo.exec;
+
+import org.codehaus.plexus.util.StringOutputStream;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.artifact.repository.DefaultArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+
+import java.io.PrintStream;
+import java.io.File;
+
+/**
+ * @author Jerome Lacoste <jerome@coffeebreaks.org>
+ * @version $Id$
+ */
+public class ExecJavaMojoTest extends AbstractMojoTestCase
+{
+    /**
+     * Check that a simple execution with no arguments and no system properties produces the expected result
+     *
+     * we load the config from a pom file and fill up the MavenProject property ourselves
+     */
+    public void testSimpleRun() throws Exception
+    {
+        File pom = new File( getBasedir(), "src/test/projects/project4/pom.xml" );
+
+        ExecJavaMojo mojo;
+
+        mojo = (ExecJavaMojo) lookupMojo( "java", pom );
+
+        setUpProject( pom, mojo );
+
+        MavenProject project = (MavenProject) getVariableValueFromObject( mojo, "project" );
+
+        // why isn't this set up by the harmess based on the default-value?
+        setVariableValueToObject( mojo, "killAfter", new Long(-1) );
+
+        assertNotNull( mojo );
+        assertNotNull( project );
+
+        // trap System.out
+        PrintStream out = System.out;
+        StringOutputStream stringOutputStream = new StringOutputStream();
+        System.setOut(new PrintStream(stringOutputStream));
+
+        try {
+            mojo.execute();
+        } catch (Throwable e) {
+            e.printStackTrace(System.err);
+            fail(e.getMessage());
+        } finally {
+            System.setOut(out);
+        }
+
+        assertEquals("Hello\n", stringOutputStream.toString());
+    }
+
+    private void setUpProject( File pomFile, AbstractMojo mojo ) throws Exception
+    {
+        MavenProjectBuilder builder = (MavenProjectBuilder) lookup( MavenProjectBuilder.ROLE );
+
+        ArtifactRepositoryLayout localRepositoryLayout =
+            (ArtifactRepositoryLayout) lookup(
+                ArtifactRepositoryLayout.ROLE, "default"
+            );
+
+        String path = "src/test/repository";
+
+        ArtifactRepository localRepository =
+            new DefaultArtifactRepository(
+                "local", "file://" + new File( path ).getAbsolutePath(),
+                localRepositoryLayout
+            );
+
+        MavenProject project = builder.buildWithDependencies( pomFile ,
+                                                              localRepository, null );
+        setVariableValueToObject( mojo, "project", project );
+    }
+}
