4b877cf95e57093429d54912e42162c5ac463e24
Robert Scholte <rfscholte@apache.org>
2010-05-16 21:04:35 +0000

    Fix for MEXEC-75: Encoding of environment variables
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/exec-maven-plugin@12174 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/test/java/org/codehaus/plexus/util/interpolation/Interpolator.java
@@ -0,0 +1,15 @@
+package org.codehaus.plexus.util.interpolation;
+
+/**
+ * COPIED FROM plexus-utils-1.5.15 TO SATISFY TESTS
+ * 
+ * Interpolator interface. Based on existing RegexBasedInterpolator interface.
+ *
+ * @author cstamas
+ * @deprecated Use plexus-interpolation APIs instead.
+ * @version $Id$
+ */
+public interface Interpolator
+    extends org.codehaus.plexus.interpolation.Interpolator
+{
+}
---
+++ src/test/java/org/codehaus/plexus/util/interpolation/ValueSource.java
@@ -0,0 +1,13 @@
+package org.codehaus.plexus.util.interpolation;
+
+/**
+ * COPIED FROM plexus-utils-1.5.15 TO SATISFY TESTS
+ * 
+ * @author jdcasey
+ * @deprecated Use plexus-interpolation APIs instead.
+ * @version $Id$
+ */
+public interface ValueSource  extends org.codehaus.plexus.interpolation.ValueSource
+{
+
+}
