f1a352da15ac2470bad07d3f823f41434807b2c7
Karl Heinz Marbaise <khmarbaise@apache.org>
2014-01-12 14:58:49 +0000

    - Cleaned up source code
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/exec-maven-plugin@19248 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/test/java/org/codehaus/mojo/exec/ExecMojoTest.java
+++ src/test/java/org/codehaus/mojo/exec/ExecMojoTest.java
@@ -16,35 +16,32 @@ package org.codehaus.mojo.exec;
  * limitations under the License.
  */
 
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.DefaultArtifactRepository;
-import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.logging.SystemStreamLog;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectBuilder;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import org.codehaus.plexus.util.StringOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.OS;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.DefaultArtifactRepository;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.monitor.logging.DefaultLog;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.SystemStreamLog;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.logging.console.ConsoleLogger;
-import org.apache.maven.monitor.logging.DefaultLog;
-import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.codehaus.plexus.util.StringOutputStream;
 
 /**
  * @author Jerome Lacoste <jerome@coffeebreaks.org>
@@ -438,65 +435,4 @@ public class ExecMojoTest
         return result;
     }
 
-    // TAKEN FROM NetbeansFreeformPluginTest - refactor ?
-
-    /**
-     * This method asserts that the two given files are equals in their
-     * content.
-     *
-     * @param mavenRepo    Not used.
-     * @param expectedFile The file that is expected.
-     * @param actualFile   The file that is.
-     * @throws java.io.IOException if something goes wrong.
-     */
-    private void assertFileEquals( String mavenRepo, File expectedFile, File actualFile )
-        throws IOException
-    {
-        List expectedLines = getLines( mavenRepo, expectedFile );
-
-        List actualLines = getLines( mavenRepo, actualFile );
-
-        for ( int i = 0; i < expectedLines.size(); i++ )
-        {
-            String expected = expectedLines.get( i ).toString();
-
-            if ( actualLines.size() < i )
-            {
-                fail( "Too few lines in the actual file. Was " + actualLines.size() + ", expected: " +
-                    expectedLines.size() );
-            }
-
-            String actual = actualLines.get( i ).toString();
-
-            assertEquals( "Checking line #" + ( i + 1 ), expected, actual );
-        }
-
-        assertTrue( "Unequal number of lines.", expectedLines.size() == actualLines.size() );
-    }
-
-    /**
-     * This method gives the list of String in a file.
-     *
-     * @param mavenRepo Not used.
-     * @param file      The file to be read.
-     * @return The list of the lines of the file.
-     * @throws java.io.IOException if something goes wrong.
-     */
-    private List getLines( String mavenRepo, File file )
-        throws IOException
-    {
-        List lines = new ArrayList();
-
-        BufferedReader reader = new BufferedReader( new FileReader( file ) );
-
-        String line;
-
-        while ( ( line = reader.readLine() ) != null )
-        {
-            lines.add(
-                line ); //StringUtils.replace( line, "#ArtifactRepositoryPath#", mavenRepo.replace( '\\', '/' ) ) );
-        }
-
-        return lines;
-    }
 }
