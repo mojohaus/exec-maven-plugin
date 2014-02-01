a184453fe24d18cd7dfbfdb9eb2e9194cccb7816
Karl Heinz Marbaise <khmarbaise@apache.org>
2014-02-01 22:58:01 +0000

    [MEXEC-108]   - Added appropriate warnings     cause the bug is caused by commons-exec.     These are only work a rounds.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/exec-maven-plugin@19359 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/exec/ExecMojo.java
+++ src/main/java/org/codehaus/mojo/exec/ExecMojo.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -71,16 +70,16 @@ public class ExecMojo
 {
     /**
      * <p>
-     * The executable. Can be a full path or the name of the executable. In the latter case, the executable must be in the
-     * PATH for the execution to work.
+     * The executable. Can be a full path or the name of the executable. In the latter case, the executable must be in
+     * the PATH for the execution to work.
      * </p>
      * <p>
      * The plugin will search for the executable in the following order:
      * <ol>
-     *  <li>relative to the root of the project</li>
-     *  <li>as toolchain executable</li>
-     *  <li>relative to the working directory (Windows only)</li>
-     *  <li>relative to the directories specified in the system property PATH (Windows Only)</li>
+     * <li>relative to the root of the project</li>
+     * <li>as toolchain executable</li>
+     * <li>relative to the working directory (Windows only)</li>
+     * <li>relative to the directories specified in the system property PATH (Windows Only)</li>
      * </ol>
      * Otherwise use the executable as is.
      * </p>
@@ -101,9 +100,9 @@ public class ExecMojo
 
     /**
      * Program standard and error output will be redirected to the file specified by this optional field. If not
-     * specified the standard Maven logging is used.
-     * <br/>
-     * <strong>Note:</strong> Be aware that <code>System.out</code> and <code>System.err</code> use buffering, so don't rely on the order!
+     * specified the standard Maven logging is used. <br/>
+     * <strong>Note:</strong> Be aware that <code>System.out</code> and <code>System.err</code> use buffering, so don't
+     * rely on the order!
      * 
      * @parameter expression="${exec.outputFile}"
      * @since 1.1-beta-2
@@ -114,9 +113,8 @@ public class ExecMojo
 
     /**
      * <p>
-     *   A list of arguments passed to the {@code executable}, 
-     *   which should be of type <code>&lt;argument&gt;</code> or <code>&lt;classpath&gt;</code>.
-     *   Can be overridden by using the <code>exec.args</code> environment variable.
+     * A list of arguments passed to the {@code executable}, which should be of type <code>&lt;argument&gt;</code> or
+     * <code>&lt;classpath&gt;</code>. Can be overridden by using the <code>exec.args</code> environment variable.
      * </p>
      * 
      * @parameter
@@ -126,11 +124,9 @@ public class ExecMojo
 
     /**
      * <p>
-     * This will control if you like to fail the build if
-     * an arguments element is empty. This means that for arguments 
-     * which would evaluate to <code>Null</code> an
-     * <code>""</code> empty string will be added to the command line
-     * of the {@code executable} which will be called.
+     * This will control if you like to fail the build if an arguments element is empty. This means that for arguments
+     * which would evaluate to <code>Null</code> an <code>""</code> empty string will be added to the command line of
+     * the {@code executable} which will be called.
      * </p>
      * 
      * @parameter default-value="true"
@@ -138,6 +134,18 @@ public class ExecMojo
      * @see #arguments
      */
     private boolean failWithEmptyArgument;
+
+    /**
+     * <p>
+     * The following will control if you like to get a warning during the build if an entry either key/value of
+     * environmentVariables will be evaluated to <code>Null</code> an <code>""</code> empty string will be used instead.
+     * </p>
+     * 
+     * @parameter default-value="true"
+     * @since 1.3
+     * @see #environmentVariables
+     */
+    private boolean failWithNullKeyOrValueInEnvironment;
 
     /**
      * @parameter default-value="${basedir}"
@@ -197,7 +205,7 @@ public class ExecMojo
             getLog().info( "skipping execute as per configuraion" );
             return;
         }
-        
+
         if ( basedir == null )
         {
             throw new IllegalStateException( "basedir is null. Should not be possible." );
@@ -207,24 +215,24 @@ public class ExecMojo
         {
 
             handleWorkingDirectory();
-            
+
             String argsProp = getSystemProperty( "exec.args" );
 
             List<String> commandArguments = new ArrayList<String>();
 
             if ( hasCommandlineArgs() )
             {
-                handleCommandLineArgs(commandArguments);
+                handleCommandLineArgs( commandArguments );
             }
             else if ( !StringUtils.isEmpty( argsProp ) )
             {
-                handleSystemPropertyArguments(argsProp, commandArguments);
+                handleSystemPropertyArguments( argsProp, commandArguments );
             }
             else
             {
                 if ( arguments != null )
                 {
-                    handleArguments(commandArguments);
+                    handleArguments( commandArguments );
                 }
             }
 
@@ -232,28 +240,28 @@ public class ExecMojo
 
             CommandLine commandLine = getExecutablePath( enviro, workingDirectory );
 
-            String[] args = commandArguments.toArray(new String[commandArguments.size()]);
+            String[] args = commandArguments.toArray( new String[commandArguments.size()] );
 
             commandLine.addArguments( args, false );
 
             Executor exec = new DefaultExecutor();
             exec.setWorkingDirectory( workingDirectory );
-            fillSuccessCodes(exec);
+            fillSuccessCodes( exec );
 
             getLog().debug( "Executing command line: " + commandLine );
 
             try
             {
                 int resultCode;
-                if( outputFile != null )
+                if ( outputFile != null )
                 {
                     if ( !outputFile.getParentFile().exists() && !outputFile.getParentFile().mkdirs() )
                     {
                         getLog().warn( "Could not create non existing parent directories for log file: " + outputFile );
                     }
-                    
+
                     FileOutputStream outputStream = null;
-                    try 
+                    try
                     {
                         outputStream = new FileOutputStream( outputFile );
 
@@ -293,13 +301,13 @@ public class ExecMojo
         }
     }
 
-    private Map<String, String> handleSystemEnvVariables()
+    private Map<String, String> handleSystemEnvVariables() throws MojoExecutionException
     {
         Map<String, String> enviro = new HashMap<String, String>();
         try
         {
             Properties systemEnvVars = CommandLineUtils.getSystemEnvVars();
-            for( Map.Entry<?, ?> entry : systemEnvVars.entrySet() )
+            for ( Map.Entry<?, ?> entry : systemEnvVars.entrySet() )
             {
                 enviro.put( (String) entry.getKey(), (String) entry.getValue() );
             }
@@ -311,21 +319,49 @@ public class ExecMojo
 
         if ( environmentVariables != null )
         {
+            for ( Map.Entry<String, String> item : environmentVariables.entrySet() )
+            {
+                getLog().debug( "Entry: key:" + item.getKey() + " value:" + item.getValue() );
+
+                // The following checks are only in relationship with MEXEC-108
+                // (https://issues.apache.org/jira/browse/EXEC-80)
+                if ( item.getKey() == null )
+                {
+                    if ( failWithNullKeyOrValueInEnvironment )
+                    {
+                        throw new MojoExecutionException(
+                                                         "The defined environment contains an entry with null key. This could cause failures." );
+                    }
+                    else
+                    {
+                        getLog().warn( "The defined environment contains an entry with null key. This could cause failures." );
+                    }
+                }
+                if ( item.getValue() == null )
+                {
+                    if ( failWithNullKeyOrValueInEnvironment )
+                    {
+                        throw new MojoExecutionException(
+                                                         "The defined environment contains an entry with null value (key:"
+                                                             + item.getKey() + "). This could cause failures." );
+                    }
+                    else
+                    {
+                        getLog().warn( "The defined environment contains an entry with null value (key:" + item.getKey()
+                                       + "). This could cause failures." );
+                    }
+                }
+            }
             enviro.putAll( environmentVariables );
         }
         return enviro;
     }
 
     /**
-     * This method is a convenient method to make
-     * the execute method a little bit more readable.
-     *
-     * It will define the workingDirectory to be the
-     * baseDir in case of workingDirectory is null.
-     *
-     * If the workingDirectory does not exist
-     * it will created.
-     *
+     * This method is a convenient method to make the execute method a little bit more readable. It will define the
+     * workingDirectory to be the baseDir in case of workingDirectory is null. If the workingDirectory does not exist it
+     * will created.
+     * 
      * @throws MojoExecutionException
      */
     private void handleWorkingDirectory()
@@ -347,13 +383,15 @@ public class ExecMojo
         }
     }
 
-    private void handleSystemPropertyArguments(String argsProp, List<String> commandArguments) throws MojoExecutionException {
+    private void handleSystemPropertyArguments( String argsProp, List<String> commandArguments )
+        throws MojoExecutionException
+    {
         getLog().debug( "got arguments from system properties: " + argsProp );
 
         try
         {
-            String[] args = CommandLineUtils.translateCommandline(argsProp);
-            commandArguments.addAll( Arrays.asList(args) );
+            String[] args = CommandLineUtils.translateCommandline( argsProp );
+            commandArguments.addAll( Arrays.asList( args ) );
         }
         catch ( Exception e )
         {
@@ -361,7 +399,9 @@ public class ExecMojo
         }
     }
 
-    private void handleCommandLineArgs(List<String> commandArguments) throws MojoExecutionException, IOException {
+    private void handleCommandLineArgs( List<String> commandArguments )
+        throws MojoExecutionException, IOException
+    {
         String[] args = parseCommandlineArgs();
         for ( int i = 0; i < args.length; i++ )
         {
@@ -387,21 +427,26 @@ public class ExecMojo
         }
     }
 
-    private void handleArguments(List<String> commandArguments) throws MojoExecutionException, IOException {
+    private void handleArguments( List<String> commandArguments )
+        throws MojoExecutionException, IOException
+    {
         for ( int i = 0; i < arguments.size(); i++ )
         {
             Object argument = arguments.get( i );
             String arg;
             if ( argument == null )
             {
-            	if ( failWithEmptyArgument ) {
-            		throw new MojoExecutionException( "Misconfigured argument (" + (i + 1) + "), value is null. "
-            				+ "Set the argument to an empty value if this is the required behaviour." );
-            		
-            	} else {
-            		//Just add an empty string to the argument list.
+                if ( failWithEmptyArgument )
+                {
+                    throw new MojoExecutionException( "Misconfigured argument (" + ( i + 1 ) + "), value is null. "
+                        + "Set the argument to an empty value if this is the required behaviour." );
+
+                }
+                else
+                {
+                    // Just add an empty string to the argument list.
                     commandArguments.add( "" );
-            	}
+                }
             }
             else if ( argument instanceof String && isLongClassPathArgument( (String) argument ) )
             {
@@ -411,8 +456,7 @@ public class ExecMojo
                 // NOTE: the jar will contain the classpath and the main class
                 commandArguments.add( "-jar" );
                 File tmpFile =
-                    createJar( computeClasspath( (Classpath) arguments.get( i + 1 ) ),
-                               (String) arguments.get( i + 2 ) );
+                    createJar( computeClasspath( (Classpath) arguments.get( i + 1 ) ), (String) arguments.get( i + 2 ) );
                 commandArguments.add( tmpFile.getAbsolutePath() );
                 i += 2;
             }
@@ -441,7 +485,7 @@ public class ExecMojo
 
     boolean isResultCodeAFailure( int result )
     {
-        if ( successCodes == null || successCodes.length == 0 ) 
+        if ( successCodes == null || successCodes.length == 0 )
         {
             return result != 0;
         }
@@ -465,8 +509,8 @@ public class ExecMojo
      * plugin cannot know from maven the phase it is executed in. So we have to depend on the user to tell us he wants
      * the scope in which the plugin is expected to be executed.
      * 
-     * @param specifiedClasspath Non null when the user restricted the dependencies, 
-     *   <code>null</code> otherwise (the default classpath will be used)
+     * @param specifiedClasspath Non null when the user restricted the dependencies, <code>null</code> otherwise (the
+     *            default classpath will be used)
      * @return a platform specific String representation of the classpath
      */
     private String computeClasspathString( Classpath specifiedClasspath )
@@ -487,8 +531,8 @@ public class ExecMojo
      * plugin cannot know from maven the phase it is executed in. So we have to depend on the user to tell us he wants
      * the scope in which the plugin is expected to be executed.
      * 
-     * @param specifiedClasspath Non null when the user restricted the dependencies, <code>null</code> 
-     *    otherwise (the default classpath will be used)
+     * @param specifiedClasspath Non null when the user restricted the dependencies, <code>null</code> otherwise (the
+     *            default classpath will be used)
      * @return a list of class path elements
      */
     private List<String> computeClasspath( Classpath specifiedClasspath )
@@ -554,7 +598,7 @@ public class ExecMojo
             getLog().debug( "Toolchains are ignored, 'executable' parameter is set to " + executable );
             exec = execFile.getAbsolutePath();
         }
-        
+
         if ( exec == null )
         {
             Toolchain tc = getToolchain();
@@ -577,7 +621,7 @@ public class ExecMojo
                     {
                         exec = ex;
                     }
-                    
+
                     if ( exec == null )
                     {
                         // now try to figure the path from PATH, PATHEXT env vars
@@ -630,8 +674,8 @@ public class ExecMojo
     // methods used for tests purposes - allow mocking and simulate automatic setters
     //
 
-    protected int executeCommandLine( Executor exec, CommandLine commandLine, Map<String, String> enviro, OutputStream out,
-                                      OutputStream err )
+    protected int executeCommandLine( Executor exec, CommandLine commandLine, Map<String, String> enviro,
+                                      OutputStream out, OutputStream err )
         throws ExecuteException, IOException
     {
         BufferedOutputStream bosStdOut = new BufferedOutputStream( out );
@@ -655,7 +699,7 @@ public class ExecMojo
         }
         return result;
     }
-    
+
     protected int executeCommandLine( Executor exec, CommandLine commandLine, Map<String, String> enviro,
                                       FileOutputStream outputFile )
         throws ExecuteException, IOException
@@ -678,7 +722,6 @@ public class ExecMojo
         }
         return result;
     }
-
 
     void setExecutable( String executable )
     {
