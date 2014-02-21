dc960519b51a62d2fc6c3b8a3f0dbe0e410df19d
Karl Heinz Marbaise <khmarbaise@apache.org>
2014-02-21 21:27:10 +0000

    - Make integration tests Maven version aware to   distinguish between different maven versions.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/exec-maven-plugin@19444 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/multiple-executions/verify.groovy
+++ src/it/multiple-executions/verify.groovy
@@ -43,17 +43,52 @@ def getProjectVersion() {
    
       return pom.version;
 }
+def getMavenVersion(buildLog) {
+    def maven = null;
+    buildLog.eachLine { line ->
+        if (line.startsWith("Apache Maven 2.2.1")) {
+            maven = "2.2.1";
+        } else if (line.startsWith("Apache Maven 3.0.3")) {
+            maven = "3.0.3";
+        } else if (line.startsWith("Apache Maven 3.0.4")) {
+            maven = "3.0.4";
+        } else if (line.startsWith("Apache Maven 3.0.5")) {
+            maven = "3.0.5";
+        } else if (line.startsWith("Apache Maven 3.1.0")) {
+            maven = "3.1.0";
+        } else if (line.startsWith("Apache Maven 3.1.1")) {
+            maven = "3.1.1";
+        } else if (line.startsWith("Apache Maven 3.2.1")) {
+            maven = "3.2.1";
+        }
+    }
+
+    return maven
+}
+
+def mavenVersion = getMavenVersion(buildLog)
+
    
 def projectVersion = getProjectVersion();
 def pluginVersion = getPluginVersion();
 
 println "Project version: ${projectVersion}"
 println "Plugin version ${pluginVersion}"
-   
-t.checkExistenceAndContentOfAFile(buildLog, [
-  "[DEBUG]   (f) failWithEmptyArgument = true",
-  "[DEBUG]   (f) arguments = [-cp, target/classes, Main]",
-  "[INFO] --- exec-maven-plugin:" + pluginVersion + ":exec (first-execution) @ multiple-execution ---",
-  "[INFO] --- exec-maven-plugin:" + pluginVersion + ":exec (second-execution) @ multiple-execution ---",
-  "[INFO] --- exec-maven-plugin:" + pluginVersion + ":exec (third-execution) @ multiple-execution ---",
-])
+
+if (mavenVersion.equals("2.2.1")) {
+  t.checkExistenceAndContentOfAFile(buildLog, [
+    "[DEBUG]   (f) failWithEmptyArgument = true",
+    "[DEBUG]   (f) arguments = [-cp, target/classes, Main]",
+    "[INFO] [exec:exec {execution: first-execution}]",
+    "[INFO] [exec:exec {execution: second-execution}]",
+    "[INFO] [exec:exec {execution: third-execution}]",
+  ])
+} else {
+  t.checkExistenceAndContentOfAFile(buildLog, [
+    "[DEBUG]   (f) failWithEmptyArgument = true",
+    "[DEBUG]   (f) arguments = [-cp, target/classes, Main]",
+    "[INFO] --- exec-maven-plugin:" + pluginVersion + ":exec (first-execution) @ multiple-execution ---",
+    "[INFO] --- exec-maven-plugin:" + pluginVersion + ":exec (second-execution) @ multiple-execution ---",
+    "[INFO] --- exec-maven-plugin:" + pluginVersion + ":exec (third-execution) @ multiple-execution ---",
+  ])
+}
