5eb78741d663850952c97c27d00658ca59fd14e1
Adrian Price <demonfiddler@virginmedia.com>
2016-01-19 12:47:11 +0000

    Avoid travis-ci gmaven-plugin provisioning error during integration test

--- src/it/async/src/main/scripts/async-it.cmd
+++ src/it/async/src/main/scripts/async-it.cmd
@@ -2,3 +2,4 @@
 echo async exec:exec test
 rem Block, so that we're still executing when the Maven JVM shuts down and destroys our shell process
 pause > nul
+echo async exec:exec post-test
--- src/it/async/verify.groovy
+++ src/it/async/verify.groovy
@@ -19,3 +19,5 @@
 File log = new File(basedir, 'build.log')
 assert log.exists()
 assert log.getText().contains( "async exec:exec test" )
+assert !log.getText().contains( "async exec:exec post-test" )
+assert log.getText().contains( "post-async-it" )
