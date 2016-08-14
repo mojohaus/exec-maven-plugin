5ce992a14a83214c3b02f9fd6d7afcf152e3c05b
Shannon Carey <rehevkor5@gmail.com>
2016-08-14 16:41:08 -0500

    Add "provided" classpathScope (runtime+provided)
    - New "provided" value for classpathScope includes both runtime and
      provided dependencies
    - Closes #60

---
+++ src/it/projects/git-issue-60-exec/src/main/java/gitissue60/Main.java
@@ -0,0 +1,31 @@
+package gitissue60;
+
+/*
+ * Copyright 2005 The Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @version $Id$
+ */
+public class Main
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger( Main.class );
+    public static void main( String[] args ) throws Exception {
+        LOGGER.info( "Can you hear me, Major Tom?" );
+    }
+}
