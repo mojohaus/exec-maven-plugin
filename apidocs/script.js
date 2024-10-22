92b7746f0dc5f5d77b022ab01d0468c7f46cc325
Slawomir Jaranowski <s.jaranowski@gmail.com>
2024-10-22 09:49:01 +0200

    Site checkin for project Exec Maven Plugin

--- apidocs/script.js
+++ apidocs/script.js
@@ -37,7 +37,7 @@ var tableTab = "table-tab";
 var activeTableTab = "active-table-tab";
 
 function loadScripts(doc, tag) {
-    createElem(doc, tag, 'script-files/search.js');
+    createElem(doc, tag, 'search.js');
 
     createElem(doc, tag, 'module-search-index.js');
     createElem(doc, tag, 'package-search-index.js');
@@ -157,7 +157,7 @@ function show(tableId, selected, columns) {
 }
 
 function updateTabs(tableId, selected) {
-    document.querySelector('div#' + tableId +' .summary-table')
+    document.getElementById(tableId + '.tabpanel')
         .setAttribute('aria-labelledby', selected);
     document.querySelectorAll('button[id^="' + tableId + '"]')
         .forEach(function(tab, index) {
@@ -227,10 +227,27 @@ function switchCopyLabel(button, span) {
         }, 100);
     }, 1900);
 }
-// Dynamically set scroll margin to accomodate for draft header
+// Workaround for scroll position not being included in browser history (8249133)
 document.addEventListener("DOMContentLoaded", function(e) {
-    document.querySelectorAll(':not(input)[id]').forEach(
-        function(c) {
-            c.style["scroll-margin-top"] = Math.ceil(document.querySelector("header").offsetHeight) + "px"
-        });
+    var contentDiv = document.querySelector("div.flex-content");
+    window.addEventListener("popstate", function(e) {
+        if (e.state !== null) {
+            contentDiv.scrollTop = e.state;
+        }
+    });
+    window.addEventListener("hashchange", function(e) {
+        history.replaceState(contentDiv.scrollTop, document.title);
+    });
+    var timeoutId;
+    contentDiv.addEventListener("scroll", function(e) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(function() {
+            history.replaceState(contentDiv.scrollTop, document.title);
+        }, 100);
+    });
+    if (!location.hash) {
+        history.replaceState(contentDiv.scrollTop, document.title);
+    }
 });
