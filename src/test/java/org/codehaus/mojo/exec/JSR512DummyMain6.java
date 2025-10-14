7a66c3ec712ec1f525e7409de46a72a7e904be15
anurag.ag <anuragagarwal561994@users.noreply.github.com>
2025-10-14 06:04:13 +0530

    Add support for JEP 512 for for package-private static main methods with and without arguments

---
+++ src/test/java/org/codehaus/mojo/exec/JSR512DummyMain6.java
@@ -0,0 +1,10 @@
+package org.codehaus.mojo.exec;
+
+/**
+ * Tests a main method that's not public, static, and has no parameters.
+ */
+public class JSR512DummyMain6 {
+    static void main() {
+        System.out.println("Correct choice");
+    }
+}
